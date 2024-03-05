453c9c406554b48a3ccd09ef29d114cc63ff8b2d
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-03-05 02:20:29 +0200

    Add files via upload

---
+++ xtraEvent_v6.2.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+ # 
+echo ""
+cd /tmp
+curl  -k -Lbk -m 55532 -m 555104 "https://raw.githubusercontent.com/Ham-ahmed/EPG/main/xtraevent-6.2.tar.gz" > /tmp/xtraevent-6.2.tar.gz
+sleep 1
+echo "installing ...."
+cd /tmp
+tar -xzf xtraevent-6.2.tar.gz  -C /
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 1
+cd
+rm -f /tmp/xtraevent-6.2.tar.gz
+echo "OK"
+echo " UPLOADED BY Hamdy Ahmed
+sleep 4;
+echo ""
+echo ""
+exit
+
+
+
+
+
